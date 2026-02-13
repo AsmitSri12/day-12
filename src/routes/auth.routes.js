565d6b3e48f46fa5d14e887fdb8897ffba996b4a
AsmitSri12 <asmitsri2182@gmail.com>
2026-02-13 09:37:31 +0530

    Add user existence check during registration

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,6 +7,14 @@ const authRouter = express.Router()
 authRouter.post("/register", async (req, res) => {
     const {name, email, password} = req.body
 
+    const isUserAlreadyExists = await userModel.findOne({email})
+
+    if(isUserAlreadyExists) {
+        return res.status(409).json({
+            message: "Account already exists with the same email address"
+        })
+    }
+
     const user = await userModel.create({
         name, email, password
     })
